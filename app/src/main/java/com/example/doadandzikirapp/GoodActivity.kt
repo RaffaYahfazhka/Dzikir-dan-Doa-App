6a4b71434ea9a5d20ef4fe72aa7a692c09ef9753
Raffa Yahfazhka <mraffayahfazhka@gmail.com>
2021-11-10 08:20:13 +0700

    Push dan commit

---
+++ app/src/main/java/com/example/doadandzikirapp/GoodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.doadandzikirapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.good)
+    }
+}
